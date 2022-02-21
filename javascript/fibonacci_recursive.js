795cc3970bbcd0ef1a332716f7acbc73266c314c
Daniel Calvin <arockne2003@yahoo.com>
2022-02-21 12:28:00 -0700

    commit tree d9ee453028fb7600081f2647686c7243743c7d52

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,21 +1,34 @@
+//i number
+//o nth element fibonacci series
+
 function fibonacci(n) {
-  // type your code here
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  console.log(n)
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+  // console.log("Expecting: 0");
+  // console.log("=>", fibonacci(0));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+  // console.log("Expecting: 1");
+  // console.log("=>", fibonacci(2));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+  // console.log("Expecting: 55");
+  // console.log("=>", fibonacci(10));
+
+  console.log(fibonacci(3))
 }
 
 module.exports = fibonacci;
